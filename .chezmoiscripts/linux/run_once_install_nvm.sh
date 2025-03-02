f242d2e34c93911e61a3cbedce06980de97edd8d
CeansBzh <ebriantais@orange.fr>
2025-03-02 11:23:23 +0100

    Ajoute l'installation de NVM

---
+++ .chezmoiscripts/linux/run_once_install_nvm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if NVM is already installed
+if [ ! -d "$HOME/.nvm" ]; then
+  # Download and install NVM
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+  # Load NVM for immediate use
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+  # Install the latest version of Node.js
+  nvm install node
+fi
